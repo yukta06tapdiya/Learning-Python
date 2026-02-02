ba43e4aad322cb2b1c747128661ab60b748bfa00
Yukta Tapdiya <yukta06tapdiya@gmail.com>
2026-02-02 19:30:14 +0530

    Add files via upload
    String Slicing And Indexing

---
+++ Program5.py
@@ -0,0 +1,8 @@
+#String slicing and Indexing
+name="Team India"
+print(name[0:5])#slicing#includind 0 but not 5
+print(name[0:-4])
+print(name[-9:-2])
+print(name[2])#indexing
+print(len(name))#length of string
+print(name[-4:-2])
