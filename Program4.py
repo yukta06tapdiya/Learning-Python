f642a39ec2b3ca4030e7287babb1812fe963c008
Yukta Tapdiya <yukta06tapdiya@gmail.com>
2026-02-02 18:13:31 +0530

    Add files via upload
    Typecasting in Python

---
+++ Program4.py
@@ -0,0 +1,13 @@
+#Explicit Typecasting
+#Developer to Python
+string="2"
+num=10
+string_num=int(string)
+sum=num+string_num
+print(sum)
+
+#Implicit Typecasting
+#Python Automatically converts
+a=2.22
+b=4
+print(a+b)
