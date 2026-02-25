c45c65fdcd3cfe7183d5f11edba3b50f0a2d970a
Yukta Tapdiya <yukta06tapdiya@gmail.com>
2026-02-25 22:22:19 +0530

    Add files via upload
    f-strings

---
+++ Program15.py
@@ -0,0 +1,10 @@
+#f-strings
+letter="Hey my name is {} and I am from {}"
+country="India"
+name="Jyotsna"
+
+print(f"Hey my name is {name} and I am from {country}")
+
+price=49.0990
+txt=f"For only {price:.2f} dollars!"
+print(txt)
